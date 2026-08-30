b4354c0c814b051cecdab73aa714567317e9ed52
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-30 14:53:17 +0400

    Task 2.2.1 Add PhoneNumberCreateRequest

---
+++ requestobjects/PhoneNumberCreateRequest.java
@@ -0,0 +1,13 @@
+package com.cl.demo.requestobjects;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhoneNumberCreateRequest {
+
+    private String countryCode;
+
+    private Long phoneNumber;
+}
